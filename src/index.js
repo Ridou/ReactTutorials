a03edce7cc00ea5eb1a79e535c661e2ed74a51b7
Ridou <richard.santin@gmail.com>
2017-01-27 17:06:16 -0800

    Woohoo React

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyBrXSGGozPUWnJ1TamirzLWelLKHCvLIcc';
+
+//Youtube API Search
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+        
+        YTSearch({key: API_KEY, term: 'iphone'}, (videos) => {
+            this.setState({videos});
+        });
+            //if the key and value have the same name you can just pass one word            
+    }
+    render() {
+        return ( 
+            <div>
+                < SearchBar />
+                < VideoDetail video={this.state.videos[0] } />
+                < VideoList videos={this.state.videos} /> 
+            </div>
+        );
+
+    }
+}
+
+
+ReactDOM.render( < App / > , document.querySelector('.container'));
